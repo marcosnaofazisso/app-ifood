63c80adbdd3e6d519905586a86d75f6074cee677
Marcos Vinicius Ferreira <marcosferreiraf22@gmail.com>
2023-01-26 21:47:05 -0300

    add home activity and auth validations

--- app/src/main/java/com/marcosviniciusferreira/ifood/activity/AutenticacaoActivity.java
+++ app/src/main/java/com/marcosviniciusferreira/ifood/activity/AutenticacaoActivity.java
@@ -1,18 +1,29 @@
 package com.marcosviniciusferreira.ifood.activity;
 
+import android.content.Intent;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Switch;
+import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 import com.marcosviniciusferreira.ifood.R;
+import com.marcosviniciusferreira.ifood.helper.ConfiguracaoFirebase;
 
 public class AutenticacaoActivity extends AppCompatActivity {
 
     private Button botaoAcessar;
     private EditText campoEmail, campoSenha;
     private Switch tipoAcesso;
+
+    private FirebaseAuth auth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,11 +34,17 @@ public class AutenticacaoActivity extends AppCompatActivity {
 
         inicializarComponentes();
 
-        if (tipoAcesso.isChecked()) {
+        auth = ConfiguracaoFirebase.getFirebaseAuth();
 
-        } else {
-            
-        }
+        botaoAcessar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String email = campoEmail.getText().toString();
+                String senha = campoSenha.getText().toString();
+
+                validarCampos(email, senha);
+            }
+        });
 
     }
 
@@ -37,5 +54,58 @@ public class AutenticacaoActivity extends AppCompatActivity {
         campoSenha = findViewById(R.id.editCadastroSenha);
         tipoAcesso = findViewById(R.id.switchAcesso);
 
+    }
+
+    private void validarCampos(String email, String senha) {
+        if (!email.isEmpty()) {
+            if (!senha.isEmpty()) {
+
+                acessarLoginOuCadastro(email, senha);
+
+            } else {
+                exibirMensagemErro("Preencha o campo senha!");
+
+            }
+        } else {
+            exibirMensagemErro("Preencha o campo email!");
+        }
+    }
+
+    private void exibirMensagemErro(String mensagem) {
+        Toast.makeText(AutenticacaoActivity.this, mensagem, Toast.LENGTH_SHORT);
+
+    }
+
+    private void acessarLoginOuCadastro(String email, String senha) {
+        if (tipoAcesso.isChecked()) { //Cadastro
+
+            auth.createUserWithEmailAndPassword(email, senha).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                @Override
+                public void onComplete(@NonNull Task<AuthResult> task) {
+                    if (task.isSuccessful()) {
+                        startActivity(new Intent(getApplicationContext(), HomeActivity.class));
+                    } else {
+                        exibirMensagemErro("Erro no cadastro. Verifique e tente novamente");
+                    }
+                }
+            });
+
+        } else {//Login
+
+            auth.signInWithEmailAndPassword(email, senha).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                @Override
+                public void onComplete(@NonNull Task<AuthResult> task) {
+                    if (task.isSuccessful()) {
+
+                        startActivity(new Intent(getApplicationContext(), HomeActivity.class));
+
+                    } else {
+                        exibirMensagemErro("Erro no login. Verifique e tente novamente");
+                    }
+
+                }
+            });
+
+        }
     }
 }
---
+++ app/src/main/java/com/marcosviniciusferreira/ifood/activity/HomeActivity.java
@@ -0,0 +1,15 @@
+package com.marcosviniciusferreira.ifood.activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.marcosviniciusferreira.ifood.R;
+
+public class HomeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home);
+    }
+}
