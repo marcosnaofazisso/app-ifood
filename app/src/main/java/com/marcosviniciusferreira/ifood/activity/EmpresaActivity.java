3dcb5062a66401ecba6f5f79f259a494ba4b2862
Marcos Vinicius Ferreira <marcosferreiraf22@gmail.com>
2023-01-27 09:25:30 -0300

    add method from company activity toolbar

--- app/src/main/java/com/marcosviniciusferreira/ifood/activity/EmpresaActivity.java
+++ app/src/main/java/com/marcosviniciusferreira/ifood/activity/EmpresaActivity.java
@@ -4,16 +4,26 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 import com.marcosviniciusferreira.ifood.R;
+import com.marcosviniciusferreira.ifood.helper.ConfiguracaoFirebase;
 
 public class EmpresaActivity extends AppCompatActivity {
+
+    private FirebaseAuth auth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_empresa);
+
+        auth = ConfiguracaoFirebase.getFirebaseAuth();
 
         //Configuracoes Toolbar
         Toolbar toolbar = findViewById(R.id.toolbar);
@@ -23,8 +33,44 @@ public class EmpresaActivity extends AppCompatActivity {
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
+
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu_empresa, menu);
+
         return super.onCreateOptionsMenu(menu);
 
-        
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+
+        if (item.getItemId() == R.id.menuSair) {
+            deslogarUsuario();
+        } else if (item.getItemId() == R.id.menuConfiguracoes) {
+            abrirConfiguracoes();
+        } else if (item.getItemId() == R.id.menuNovoProduto) {
+            abrirNovoProduto();
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    private void deslogarUsuario() {
+        try {
+
+            auth.signOut();
+
+        } catch (Exception e) {
+            Log.d("ERROR", "Logout Exception: " + e.getMessage());
+
+        }
+
+    }
+
+    private void abrirConfiguracoes() {
+
+    }
+
+    private void abrirNovoProduto() {
+
     }
 }
