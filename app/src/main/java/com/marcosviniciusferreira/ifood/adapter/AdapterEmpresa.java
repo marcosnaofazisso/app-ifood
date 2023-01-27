08e772821ed2651e0d1f9793256932295aaa7f60
Marcos Vinicius Ferreira <marcosferreiraf22@gmail.com>
2023-01-27 19:56:43 -0300

    add list of restaurantes (companies) in user app

---
+++ app/src/main/java/com/marcosviniciusferreira/ifood/adapter/AdapterEmpresa.java
@@ -0,0 +1,71 @@
+package com.marcosviniciusferreira.ifood.adapter;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.marcosviniciusferreira.ifood.R;
+import com.marcosviniciusferreira.ifood.model.Empresa;
+import com.squareup.picasso.Picasso;
+
+import java.util.List;
+
+
+public class AdapterEmpresa extends RecyclerView.Adapter<AdapterEmpresa.MyViewHolder> {
+
+    private List<Empresa> empresas;
+
+    public AdapterEmpresa(List<Empresa> empresas) {
+        this.empresas = empresas;
+    }
+
+    @NonNull
+    @Override
+    public MyViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int i) {
+        View itemLista = LayoutInflater.from(parent.getContext()).inflate(R.layout.adapter_empresa, parent, false);
+        return new MyViewHolder(itemLista);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull MyViewHolder holder, int i) {
+        Empresa empresa = empresas.get(i);
+        holder.nomeEmpresa.setText(empresa.getNome());
+        holder.categoria.setText(empresa.getCategoria() + " - ");
+        holder.tempo.setText(empresa.getTempo() + " Min");
+        holder.entrega.setText("R$ " + empresa.getPrecoEntrega().toString());
+
+        //Carregar imagem
+        String urlImagem = empresa.getUrlImagem();
+        Picasso.get().load(urlImagem).into(holder.imagemEmpresa);
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return empresas.size();
+    }
+
+    public class MyViewHolder extends RecyclerView.ViewHolder {
+
+        ImageView imagemEmpresa;
+        TextView nomeEmpresa;
+        TextView categoria;
+        TextView tempo;
+        TextView entrega;
+
+        public MyViewHolder(View itemView) {
+            super(itemView);
+
+            nomeEmpresa = itemView.findViewById(R.id.textNomeEmpresa);
+            categoria = itemView.findViewById(R.id.textCategoriaEmpresa);
+            tempo = itemView.findViewById(R.id.textTempoEmpresa);
+            entrega = itemView.findViewById(R.id.textEntregaEmpresa);
+            imagemEmpresa = itemView.findViewById(R.id.imageEmpresa);
+        }
+    }
+}
