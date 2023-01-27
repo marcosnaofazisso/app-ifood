ef3e4dd99054d779157066478110c24e906a106b
Marcos Vinicius Ferreira <marcosferreiraf22@gmail.com>
2023-01-27 09:05:17 -0300

    migration to androidx, add materialsearchview lib and company activity

--- app/src/main/java/com/marcosviniciusferreira/ifood/activity/EmpresaActivity.java
+++ app/src/main/java/com/marcosviniciusferreira/ifood/activity/EmpresaActivity.java
@@ -1,7 +1,10 @@
 package com.marcosviniciusferreira.ifood.activity;
 
-import android.support.v7.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+
 import android.os.Bundle;
+import android.view.Menu;
 
 import com.marcosviniciusferreira.ifood.R;
 
@@ -11,5 +14,17 @@ public class EmpresaActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_empresa);
+
+        //Configuracoes Toolbar
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        toolbar.setTitle("Ifood - Empresa");
+        setSupportActionBar(toolbar);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        return super.onCreateOptionsMenu(menu);
+
+        
     }
 }
