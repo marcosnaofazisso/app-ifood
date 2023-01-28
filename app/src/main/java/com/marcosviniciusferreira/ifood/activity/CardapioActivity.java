6d86a1eb05899e9a2b882bd911952fae69a94a22
Marcos Vinicius Ferreira <marcosferreiraf22@gmail.com>
2023-01-28 09:19:56 -0300

    WIP

--- app/src/main/java/com/marcosviniciusferreira/ifood/activity/CardapioActivity.java
+++ app/src/main/java/com/marcosviniciusferreira/ifood/activity/CardapioActivity.java
@@ -5,8 +5,13 @@ import androidx.appcompat.widget.Toolbar;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.app.AlertDialog;
 import android.os.Bundle;
 import android.provider.ContactsContract;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.widget.ImageView;
 import android.widget.TextView;
 
@@ -17,12 +22,16 @@ import com.google.firebase.database.ValueEventListener;
 import com.marcosviniciusferreira.ifood.R;
 import com.marcosviniciusferreira.ifood.adapter.AdapterProduto;
 import com.marcosviniciusferreira.ifood.helper.ConfiguracaoFirebase;
+import com.marcosviniciusferreira.ifood.helper.UsuarioFirebase;
 import com.marcosviniciusferreira.ifood.model.Empresa;
 import com.marcosviniciusferreira.ifood.model.Produto;
+import com.marcosviniciusferreira.ifood.model.Usuario;
 import com.squareup.picasso.Picasso;
 
 import java.util.ArrayList;
 import java.util.List;
+
+import dmax.dialog.SpotsDialog;
 
 public class CardapioActivity extends AppCompatActivity {
 
@@ -35,6 +44,11 @@ public class CardapioActivity extends AppCompatActivity {
     private List<Produto> produtos = new ArrayList<>();
     private DatabaseReference firebaseRef;
 
+    private AlertDialog dialog;
+    private Usuario usuario;
+    private String idUsuarioLogado;
+    private TextView textCarrinhoQtd, textCarrinhoTotal;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -43,6 +57,7 @@ public class CardapioActivity extends AppCompatActivity {
         inicializarComponentes();
 
         firebaseRef = ConfiguracaoFirebase.getFirebaseDatabase();
+        idUsuarioLogado = UsuarioFirebase.getIdUsuario();
 
         //Recuperar empresa selecionada
         Bundle bundle = getIntent().getExtras();
@@ -73,6 +88,47 @@ public class CardapioActivity extends AppCompatActivity {
 
         recuperarCardapio();
 
+        recuperarDadosUsuario();
+
+
+    }
+
+    private void recuperarDadosUsuario() {
+
+        dialog = new SpotsDialog.Builder()
+                .setContext(this)
+                .setMessage("Carregando...")
+                .setCancelable(false)
+                .build();
+
+        dialog.show();
+
+        final DatabaseReference usuariosRef = firebaseRef.child("usuarios")
+                .child(idUsuarioLogado);
+
+        usuariosRef.addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                if (dataSnapshot.getValue() != null) {
+
+                    usuario = dataSnapshot.getValue(Usuario.class);
+                }
+
+                recuperarPedido();
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+
+            }
+        });
+
+
+    }
+
+    private void recuperarPedido() {
+
+        dialog.dismiss();
 
     }
 
@@ -103,6 +159,29 @@ public class CardapioActivity extends AppCompatActivity {
 
     }
 
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu_cardapio, menu);
+        return super.onCreateOptionsMenu(menu);
+
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+
+        if (item.getItemId() == R.id.menuPedido) {
+            finalizarPedido();
+
+        }
+        
+        return super.onOptionsItemSelected(item);
+    }
+
+    private void finalizarPedido() {
+    }
+
     private void inicializarComponentes() {
 
         recyclerProdutosCardapio = findViewById(R.id.recyclerProdutoCardapio);
@@ -111,4 +190,6 @@ public class CardapioActivity extends AppCompatActivity {
 
 
     }
+
+
 }
